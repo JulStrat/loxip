4695f2b96321263618ca15e95b9b3c1df94a14a5
JulStrat <ioulianos.kakoulidis@hotmail.com>
2020-05-06 11:53:35 +0300

    Lox equality

--- ptypes.pas
+++ ptypes.pas
@@ -15,6 +15,7 @@ type
     FValue: Boolean;
     public
     constructor Create(v: Boolean);
+    function Equals(obj: TObject): boolean; override;
     function ToString: ansistring; override;
     property value: Boolean read FValue;
   end;
@@ -25,6 +26,7 @@ type
     FValue: Double;
     public
     constructor Create(v: Double);
+    function Equals(obj: TObject): boolean; override;
     function ToString: ansistring; override;
     property value: Double read FValue;
   end;
@@ -35,6 +37,7 @@ type
     FValue: String;
     public
     constructor Create(v: String);
+    function Equals(obj: TObject): boolean; override;
     function ToString: ansistring; override;
     property value: String read FValue;
   end;
@@ -43,9 +46,23 @@ function ObjToStr(obj: TObject): ansistring;
 
 implementation
 
+uses Math;
+
 constructor TLoxBool.Create(v: Boolean);
 begin
   FValue := v;
+end;
+
+function TLoxBool.Equals(obj: TObject): boolean;
+begin
+  if obj = nil then
+    Exit(false);
+  if obj.ClassType <> TLoxBool then
+    Exit(false);
+  if self.FValue = TLoxBool(obj).FValue then
+    Result := true
+  else
+    Result := false;
 end;
 
 function TLoxBool.ToString: ansistring;
@@ -58,6 +75,18 @@ begin
   FValue := v;
 end;
 
+function TLoxNum.Equals(obj: TObject): boolean;
+begin
+  if obj = nil then
+    Exit(false);
+  if obj.ClassType <> TLoxNum then
+    Exit(false);
+  if Math.SameValue(self.FValue, TLoxNum(obj).FValue) then
+    Result := true
+  else
+    Result := false;
+end;
+
 function TLoxNum.ToString: ansistring;
 begin
   Result := Format('%f', [FValue]);
@@ -66,6 +95,18 @@ end;
 constructor TLoxStr.Create(v: String);
 begin
   FValue := v;
+end;
+
+function TLoxStr.Equals(obj: TObject): boolean;
+begin
+  if obj = nil then
+    Exit(false);
+  if obj.ClassType <> TLoxStr then
+    Exit(false);
+  if self.FValue = TLoxStr(obj).FValue then
+    Result := true
+  else
+    Result := false;
 end;
 
 function TLoxStr.ToString: ansistring;
@@ -80,6 +121,50 @@ begin
   else
     Result := obj.ToString;
 end;
+
+var
+  lba, lbb, lbc: TLoxBool;
+  lna, lnb, lnc: TLoxNum;
+  lsa, lsb, lsc: TLoxStr;
+initialization
+begin
+  WriteLn('DEBUG - Equality test.');
+  lba := TLoxBool.Create(true);
+  lbb := TLoxBool.Create(false);
+  lbc := TLoxBool.Create(true);
+
+  lna := TLoxNum.Create(1/3);
+  lnb := TLoxNum.Create(1*1.00000000001/3.0);
+  lnc := TLoxNum.Create(1/3);
+
+  lsa := TLoxStr.Create('Hello');
+  lsb := TLoxStr.Create('HellO');
+  lsc := TLoxStr.Create('Hello');
+
+  WriteLn(lba.value, ', ', lbb.value, ', ', lbc.value);
+  if lba.Equals(lbb) then WriteLn('lba == lbb')
+  else WriteLn('lba != lbb');
+  if lba.Equals(lbc) then WriteLn('lba == lbc')
+  else WriteLn('lba != lbc');
+
+  WriteLn(lna.value, ', ', lnb.value, ', ', lnc.value);
+  if lna.Equals(lnb) then WriteLn('lna == lnb')
+  else WriteLn('lna != lnb');
+  if lna.Equals(lnc) then WriteLn('lna == lnc')
+  else WriteLn('lna != lnc');
+
+  WriteLn(lsa.value, ', ', lsb.value, ', ', lsc.value);
+  if lsa.Equals(lsb) then WriteLn('lsa == lsb')
+  else WriteLn('lsa != lsb');
+  if lsa.Equals(lsc) then WriteLn('lsa == lsc')
+  else WriteLn('lsa != lsc');
+
+  FreeAndNil(lba); FreeAndNil(lbb); FreeAndNil(lbc);
+  FreeAndNil(lna); FreeAndNil(lnb); FreeAndNil(lnc);
+  FreeAndNil(lsa); FreeAndNil(lsb); FreeAndNil(lsc);
+end
+
+finalization
 
 end.
 
